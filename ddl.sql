e5fdc6e02a0c285003def101517958cdb9cdb0d2
hilmi <hilmi.hibatullah@elnusa.co.id>
2022-01-20 10:25:19 +0700

    add query for initiate db

---
+++ ddl.sql
@@ -0,0 +1,126 @@
+-- create db if not exists
+CREATE DATABASE IF NOT EXISTS `taktuku-project`;
+
+-- create table users
+DROP TABLE IF EXISTS `taktuku-project`.`users`;
+CREATE TABLE `taktuku-project`.`users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `birth_date` date NOT NULL,
+  `phone_number` varchar(20) NOT NULL,
+  `photo` varchar(255),
+  `gender_char` char(1) NOT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `updated_date` datetime DEFAULT NULL,
+  `deleted_date` datetime DEFAULT NULL,
+  PRIMARY KEY(`id`)
+);
+
+-- create table products
+DROP TABLE IF EXISTS `taktuku-project`.`products`;
+CREATE TABLE `taktuku-project`.`products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text,
+  `price` numeric NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `photo` varchar(255),
+  `created_date` datetime DEFAULT NULL,
+  `updated_date` datetime DEFAULT NULL,
+  `deleted_date` datetime DEFAULT NULL,
+  PRIMARY KEY(`id`),
+  FOREIGN KEY (`id_user`) REFERENCES users(`id`)
+);
+
+-- create table cart_items
+DROP TABLE IF EXISTS `taktuku-project`.`cart_items`;
+CREATE TABLE `taktuku-project`.`cart_items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
+  `id_product` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `sub_total` numeric NOT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `updated_date` datetime DEFAULT NULL,
+  `deleted_date` datetime DEFAULT NULL,
+  PRIMARY KEY(`id`),
+  FOREIGN KEY (`id_user`) REFERENCES users(`id`),
+  FOREIGN KEY (`id_product`) REFERENCES products(`id`)
+);
+
+-- create table credit_card
+DROP TABLE IF EXISTS `taktuku-project`.`credit_card`;
+CREATE TABLE `taktuku-project`.`credit_card` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `number` int(20) NOT NULL,
+  `cvv` int(3) NOT NULL,
+  `month` int(12) NOT NULL,
+  `year` int(4) NOT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `updated_date` datetime DEFAULT NULL,
+  `deleted_date` datetime DEFAULT NULL,
+  PRIMARY KEY(`id`)
+);
+
+-- create table transaction
+DROP TABLE IF EXISTS `taktuku-project`.`transaction`;
+CREATE TABLE `taktuku-project`.`transaction` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
+  `id_credit_card` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  `total_price` numeric NOT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `updated_date` datetime DEFAULT NULL,
+  `deleted_date` datetime DEFAULT NULL,
+  PRIMARY KEY(`id`),
+  FOREIGN KEY (`id_user`) REFERENCES users(`id`),
+  FOREIGN KEY (`id_credit_card`) REFERENCES credit_card(`id`)
+);
+
+-- create table address
+DROP TABLE IF EXISTS `taktuku-project`.`address`;
+CREATE TABLE `taktuku-project`.`address` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_transaction` int(11) NOT NULL,
+  `country` varchar(255) NOT NULL,
+  `street` varchar(20) NOT NULL,
+  `zip` int(20) NOT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `updated_date` datetime DEFAULT NULL,
+  `deleted_date` datetime DEFAULT NULL,
+  PRIMARY KEY(`id`),
+  FOREIGN KEY (`id_transaction`) REFERENCES `transaction`(`id`)
+);
+
+-- create table transaction_status
+DROP TABLE IF EXISTS `taktuku-project`.`transaction_status`;
+CREATE TABLE `taktuku-project`.`transaction_status` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) NOT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `updated_date` datetime DEFAULT NULL,
+  `deleted_date` datetime DEFAULT NULL,
+  PRIMARY KEY(`id`)
+);
+
+-- create table transaction_detail
+DROP TABLE IF EXISTS `taktuku-project`.`transaction_detail`;
+CREATE TABLE `taktuku-project`.`transaction_detail` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_transaction` int(11) NOT NULL,
+  `id_product` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `sub_total` numeric NOT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `updated_date` datetime DEFAULT NULL,
+  `deleted_date` datetime DEFAULT NULL,
+  PRIMARY KEY(`id`),
+  FOREIGN KEY (`id_transaction`) REFERENCES `transaction`(`id`),
+  FOREIGN KEY (`id_product`) REFERENCES products(`id`)
+);
